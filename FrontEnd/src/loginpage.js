dc9480559a11fe2bd9351722a09ed298c9b35a1d
Youssef Bazzi <149521713+yoyobazzi3@users.noreply.github.com>
2024-10-13 20:25:37 -0700

    Delete FrontEnd directory

--- FrontEnd/src/loginpage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const submitUsername = document.getElementById("submit-username");
-    const helloDiv = document.getElementById("hellodiv")
-    submitUsername.addEventListener("click", function(){
-        console.log("Hello World");
-        const hello = document.createAttribute("p")
-        helloDiv.textContent = "Welcome Diva :)"
-    });
-});
